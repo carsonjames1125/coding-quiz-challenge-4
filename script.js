a6904501805d3c43627c1cf6ef9aa18f5b2bfb35
carsonjames1125 <tarheelkid44@yahoo.com>
2023-05-09 17:59:07 -0400

    developed pseudocode in javascript and began idenifying html variables

---
+++ script.js
@@ -0,0 +1,29 @@
+// need to establish variables first
+// then need to work on the functions and questions that will be displayed
+// need to create a working timer
+    // need to create a deduction concept to the timer
+// need to develop a link between local storage and highscores in html
+// need to make event listeners for buttons
+// 
+// html pieces that need to be used
+var highscoresBtn = document.querySelector('#viewhighscoresbtn');
+var Timer = document.querySelector('#timer');
+var mainTitle = document.querySelector('#title-main');
+var Instructions = document.querySelector('#instructions');
+var startBtn = document.querySelector('#startbtn');
+var viewHighscores = document.querySelector('#submithighscores');
+var totalScore = document.querySelector('#totalscore');
+var highScore = document.querySelector('#highscoreinput');
+var userInput = document.querySelector('#name');
+var submitBtn = document.querySelector('#submithighscore');
+var viewHighEnd = document.querySelector('#viewhighscores');
+var gobackBtn = document.querySelector('#goback');
+var clearHighScoreBtn = document.querySelector('#clearhighscores');
+
+// generate questions for quiz
+
+var questionsTest = {
+    questions: [
+
+    ]
+}
